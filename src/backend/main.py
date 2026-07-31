0e23fed80a9c9d875bab86c8c8e15b8fe10e7d60
Jessica McRae <jmcrae@artefactual.com>
2026-07-30 18:24:17 -0700

    Remove deprecated file structure

--- src/backend/main.py
+++
@@ -1,5 +0,0 @@
-from fastapi import FastAPI
-from backend.api.routes import router
-
-app = FastAPI()
-app.include_router(router)
--- src/backend/routes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from backend.services.prediction_service import predict_email
-
-router = APIRouter()
-
-@router.post("/predict")
-def predict(data: dict):
-    return predict_email(data["text"])
